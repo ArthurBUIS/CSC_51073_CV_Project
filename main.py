030c9c8d58934e7e7e23d49d02e5e90b7b81c700
ArthurBUIS <arthur.buis1@gmail.com>
2025-12-07 16:30:45 +0100

    feat: added main file

---
+++ main.py
@@ -0,0 +1,28 @@
+# Importation des bibliothèques
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import GroupShuffleSplit
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+
+def main():
+    """
+    Pipeline d'exécution complète pour une vidéo de musculation.
+    """
+# ==============================================================================
+# Partie 1 : Lecture de la vidéo et extraction des landmarks
+# ==============================================================================
+
+# ==============================================================================
+# Partie 2 : Classification de la vidéo
+# ==============================================================================
+    
+# ==============================================================================
+# Partie 3 : Comptage des répétitions et calcul des scores
+# ==============================================================================
